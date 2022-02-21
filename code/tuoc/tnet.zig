fec36bbefdc620e34f3550f890b7a52b2b12383f
Tien Dung <dungtn@gmail.com>
2022-02-22 06:28:40 +0700

    init tnet

--- code/tuoc/tnet.zig
+++ code/tuoc/tnet.zig
@@ -1,7 +1,40 @@
 // Tuộc Net :D
 
 const std = @import("std");
+const Matrix = @import("matrix");
 
-pub fn main() !void {
-    //
+const Activation = enum { LINEAR, LOGISTIC, RELU, LRELU, SOFTMAX };
+
+// Run an activation function on each element in a matrix,
+// modifies the matrix in place
+// matrix m: Input to activation function
+// Activation a: function to run
+fn activateMatrix(m: Matrix, a: Activation) void {
+    var i: usize = 0;
+    while (i < m.rows) : (i += 1) {
+        var sum: f32 = 0;
+        var j: usize = 0;
+        while (j < m.cols) : (j += 1) {
+            var x: f32 = m.get(i, j);
+            switch (a) {
+                .LOGISTIC => {
+                    //
+                },
+                .RELU => {
+                    //
+                },
+                .LRELU => {
+                    //
+                },
+                .SOFTMAX => {
+                    //
+                },
+                else => {},
+            }
+            sum += m.get(i, j);
+        }
+        if (a == SOFTMAX) {
+            // TODO: have to normalize by sum if we are using SOFTMAX
+        }
+    }
 }
